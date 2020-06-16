03adad64643dee282e2b693afa4daca8236e0629
Nyelanioustour <shawn.k.alexander@gmail.com>
2020-06-15 21:08:21 -0400

    added another like

--- lib/app/models/post.rb
+++ lib/app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ActiveRecord::Base
     has_many :likes
     belongs_to :user
+
+    #most_likes
+
+    #keyword_search_title
+
+    #keyword_search_body
+    
+    #keyword_search_all_fields
+    
 end
--- lib/app/models/user.rb
+++ lib/app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
    has_many :likes
    has_many :posts
+
+   def self.most_likes
+      Like.all.map {|like| like.user_id}
+   end
+
+   #most_posts
+
+   #total_posts
+
+   #avg_posts
+
 end
