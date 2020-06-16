143e9b863b8152fe1f41bffc40ef0300f087e322
Issa sangare <issabmsangare@hotmail.com>
2020-06-16 12:02:07 -0400

    User methods

---
+++ lib/app/models/run.rb
@@ -0,0 +1,10 @@
+require_relative './user.rb'
+require_relative './like.rb'
+require_relative './post.rb'
+require 'pry'
+
+# mike = User.create(username: "Bill")
+# steve = User.create(username: "Steve")
+# jim = User.create(username: "Jim")
+
+# print mike
--- lib/app/models/user.rb
+++ lib/app/models/user.rb
@@ -1,15 +1,36 @@
+require 'pry'
 class User < ActiveRecord::Base
    has_many :likes
    has_many :posts
 
-   def self.most_likes
-      Like.all.map {|like| like.user_id}
-   end
+     
+      def user_post_title
+         self.posts.map {|post| post.title}
+      end
+   
+      def user_post_topic
+         self.posts.map {|post| post.topic}
+      end
 
-   #most_posts
+      def user_post_body
+         self.posts.map {|post| post.body}
+      end
 
-   #total_posts
 
-   #avg_posts
+       def percentage_posts
+         (self.posts.count / Post.all.length.to_f) * 100
+       end 
+       end
+      
+      # def self.most_likes
+      #    Like.all.map {|like| like.user_id}
+      #    # binding.pry 
+      # end
+
+      def most_posts
+         User.all.select do |user| user.posts.count == Post.all.map{|post| post.user}.map{|user| user.posts.count}.max
+        end 
+      end
+   
 
 end
